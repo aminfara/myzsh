718f932f2571190677acd2d563a21517544897c2
Ali 🚶 <ali.aminfar@gmail.com>
2021-11-12 21:09:47 +1100

    Fixed stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
 -- General vim options
 local options = {
-    mouse = 'a',
-    number = true,
-    relativenumber = true,
-    scrolloff = 3,
-    sidescrolloff = 5,
-    wrap = false,
+  mouse = 'a',
+  number = true,
+  relativenumber = true,
+  scrolloff = 3,
+  sidescrolloff = 5,
+  wrap = false,
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
