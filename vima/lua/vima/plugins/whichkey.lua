791f6058928b5eabc62c9827440e39b25f159d50
Ali 🚶 <ali.aminfar@gmail.com>
2022-01-09 20:32:14 +1100

    Vima add Telescope and git keymaps

--- vima/lua/vima/plugins/whichkey.lua
+++ vima/lua/vima/plugins/whichkey.lua
@@ -9,3 +9,28 @@ which_key.setup({
     border = 'rounded',
   },
 })
+
+which_key.register({
+  f = {
+    name = 'find',
+    b = { '<cmd>Telescope buffers<cr>', 'Buffers' },
+    f = { '<cmd>Telescope find_files<cr>', 'Files' },
+    g = { '<cmd>Telescope live_grep<cr>', 'Grep' },
+    r = { '<cmd>Telescope oldfiles<cr>', 'Recent file' },
+    s = { '<cmd>Telescope git_status<cr>', 'Git status' },
+  },
+  g = {
+    name = 'Git',
+    b = { '<cmd>Gitsigns stage_buffer<cr>', 'Stage file' },
+    d = { '<cmd>Gitsigns diffthis<cr>', 'Diff current file' },
+    n = { '<cmd>Gitsigns next_hunk<cr>', 'Next hunk' },
+    p = { '<cmd>Gitsigns prev_hunk<cr>', 'Prev hunk' },
+    r = { '<cmd>Gitsigns refresh<cr>', 'Refresh' },
+    s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage hunk' },
+    u = { '<cmd>Gitsigns undo_stage_hunk<cr>', 'Undo stage hunk' },
+  },
+  t = {
+    name = 'Toggle',
+    f = { '<cmd>NvimTreeToggle<cr>', 'NVIMTree' },
+  },
+}, { prefix = '<leader>' })
