e2304d7666aeacb5fa7b8c86fe5a63ac4aec6c9c
Ali 🚶 <ali.aminfar@gmail.com>
2023-01-06 22:17:26 +1100

    Fix vima colorscheme

--- vima/lua/vima/plugins/colorscheme.lua
+++ vima/lua/vima/plugins/colorscheme.lua
@@ -4,13 +4,12 @@ end
 
 vim.g.base16colorspace = 256
 
-local status_ok, _ = pcall(vim.cmd, 'source $HOME/.vimrc_background')
+local base16_theme = 'base16-' .. (vim.env.BASE16_THEME or 'default-dark')
+
+status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. base16_theme)
 if not status_ok then
-    status_ok, _ = pcall(vim.cmd, 'colorscheme base16-default-dark')
-    if not status_ok then
-        vim.notify('Failed to load base16 colorscheme.')
-        return
-    end
+  vim.notify('Failed to load base16 colorscheme.')
+  return
 end
 
 -- https://github.com/RRethy/nvim-base16/issues/32 fix telescope borders
