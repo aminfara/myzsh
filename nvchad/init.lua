8599c52d723b7b7fd1f202fdf02e2884556c7df6
Ali 🚶 <ali.aminfar@gmail.com>
2023-03-30 15:51:27 +1100

    Add original NvChad custom config

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,1 +1,7 @@
-require('custom.plugins')
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
