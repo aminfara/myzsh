6d780a7a34618ac897101d54cfaba55780f054ca
Ali 🚶 <ali.aminfar@gmail.com>
2023-10-06 00:36:37 +1100

    zshrc refactoring

--- zshrc.sh
+++ zshrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check machine type
+# Set MACHINE_TYPE
 case $(uname -s) in
 Linux*) MACHINE_TYPE=Linux ;;
 Darwin*)
@@ -19,18 +19,21 @@ if [ $MACHINE_TYPE = "Unknown" ]; then
   exit 1
 fi
 
+# Set HOMEBREW_DIRECTORY based on MACHINE_TYPE
+case $MACHINE_TYPE in
+Mac_aarch) export HOMEBREW_DIRECTORY=/opt/homebrew ;;
+Mac_x86) export HOMEBREW_DIRECTORY=/usr/local/Homebrew ;;
+Linux) export HOMEBREW_DIRECTORY=/home/linuxbrew/.linuxbrew ;;
+esac
+
 # CONFIGURATIONS
 ################################################################################
 
 export MYZSH_INSTALLED_DIR=$HOME/.myzsh
-export LINUXBREW_DIRECTORY=/home/linuxbrew/.linuxbrew
-export HOMEBREW_DIRECTORY_ARCH=/opt/homebrew
-export HOMEBREW_DIRECTORY_X86=/usr/local/Homebrew
-export RTX_PREFIX="$HOME/.local/share/rtx"
-export N_PREFIX="$HOME/.n"
-export NPM_DIRECTORY=$HOME/.npm
 export ANTIGEN_DIRECTORY=$HOME/.antigen
 export DOCKER_DIRECTORY="$HOME/.docker"
+export N_PREFIX="$HOME/.n"
+export NPM_DIRECTORY=$HOME/.npm
 
 export SPACESHIP_GIT_SYMBOL="⎇  "
 export SPACESHIP_DIR_TRUNC=0
@@ -53,65 +56,9 @@ print_line() {
   echo "--------------------------------------------------------------------------------"
 }
 
-[ -d $LINUXBREW_DIRECTORY/bin ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-[ -d $HOMEBREW_DIRECTORY_ARCH/bin ] && eval "$($HOMEBREW_DIRECTORY_ARCH/bin/brew shellenv)"
-[ -d $HOMEBREW_DIRECTORY_X86/bin ] && eval "$($HOMEBREW_DIRECTORY_X86/bin/brew shellenv)"
-true
-
-is_homebrew_installed() {
-  if [ ! -v BREW_INSTALLED ]
-  then
-    type brew &>/dev/null
-    export BREW_INSTALLED="$?"
-    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-  fi
-  return $BREW_INSTALLED
-}
-
-brew_install_or_upgrade() {
-  install_homebrew
-  brew ls "$@" &>/dev/null
-  if [ "$?" -eq "0" ]
-  then
-    print_line "Brew upgrading " "$@"
-    HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$@"
-  else
-    print_line "Brew installing r" "$@"
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$@"
-  fi
-}
-
-brew_uninstall() {
-  if is_homebrew_installed
-  then
-    print_line "Brew uninstalling " "$@"
-    HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall "$@"
-  else
-    echo "Homebrew is not installed"
-    return 1
-  fi
-}
-
-git_install_or_update() {
-  if [ ! -d "$1"/.git ]
-  then
-    print_line "Installing $2"
-    git clone https://github.com/"$2" "$1"
-  else
-    print_line "Updating $2"
-    pushd "$1" &>/dev/null
-    git pull
-    popd &>/dev/null
-  fi
-}
-
-# Install Homebrew
-################################################################################
-
 install_linux_build_essentials() {
   print_line "Installing Linux build essentials"
-  if command -v "yum" &> /dev/null
-  then
+  if command -v "yum" &>/dev/null; then
     # AL2
     sudo yum -y groups install "buildsys-build"
     sudo yum -y groups install "Development Tools"
@@ -121,8 +68,7 @@ install_linux_build_essentials() {
     sudo yum -y install procps-ng curl file git
   fi
 
-  if command -v "apt" &> /dev/null
-  then
+  if command -v "apt" &>/dev/null; then
     # Ubuntu
     sudo apt update
     # Homebrew on Linux https://docs.brew.sh/Homebrew-on-Linux
@@ -132,18 +78,58 @@ install_linux_build_essentials() {
   fi
 }
 
-install_homebrew() {
-  if ! is_homebrew_installed
-  then
-    if [ "$MACHINE_TYPE" = Linux ]
-    then
+# Homebrew
+################################################################################
+
+function myzsh_activate_homebrew() {
+  if [ -d "$HOMEBREW_DIRECTORY/bin" ]; then
+    eval "$("$HOMEBREW_DIRECTORY"/bin/brew shellenv)"
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    export HOMEBREW_INSTALLED=0
+  fi
+}
+
+myzsh_is_homebrew_installed() {
+  if [ ! -v HOMEBREW_INSTALLED ]; then
+    type brew &>/dev/null
+    export HOMEBREW_INSTALLED="$?"
+  fi
+  return "$HOMEBREW_INSTALLED"
+}
+
+myzsh_install_homebrew() {
+  if ! myzsh_is_homebrew_installed; then
+    if [ "$MACHINE_TYPE" = Linux ]; then
       install_linux_build_essentials
     fi
-
     print_line "Installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    activate_homebrew
-    unset BREW_INSTALLED
+    myzsh_activate_homebrew
+    unset HOMEBREW_INSTALLED
+  else
+    print_line "Homebrew is already installed"
+  fi
+}
+
+myzsh_brew_install_or_upgrade() {
+  myzsh_install_homebrew
+  brew ls "$@" &>/dev/null
+  if [ "$?" -eq "0" ]; then
+    print_line "Brew upgrading " "$@"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$@"
+  else
+    print_line "Brew installing r" "$@"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$@"
+  fi
+}
+
+myzsh_brew_uninstall() {
+  if myzsh_is_homebrew_installed; then
+    print_line "Brew uninstalling " "$@"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall "$@"
+  else
+    echo "Homebrew is not installed"
+    return 1
   fi
 }
 
@@ -151,32 +137,29 @@ install_homebrew() {
 ################################################################################
 
 myzsh_install_cli_tools() {
-  brew_install_or_upgrade openssl readline sqlite3 xz zlib tcl-tk
-  brew_install_or_upgrade git wget fd ripgrep fzf htop jq gnupg tree tmux lazygit shellcheck shfmt bottom
-  brew_install_or_upgrade -f gdu
-  brew link --overwrite gdu  # if you have coreutils installed as well
+  myzsh_brew_install_or_upgrade openssl readline sqlite3 xz zlib tcl-tk
+  myzsh_brew_install_or_upgrade git wget fd ripgrep fzf htop jq tree tmux lazygit shellcheck shfmt bottom
   # Install fzf key bindings
-  $(brew --prefix fzf)/install --key-bindings --completion --no-update-rc --no-bash --no-fish
-  [ -f $HOME/.tmux.conf ] || ln -s $MYZSH_INSTALLED_DIR/tmux.conf $HOME/.tmux.conf
-  cp $MYZSH_INSTALLED_DIR/gitconfig-template $HOME/.gitconfig
+  "$(brew --prefix fzf)/install" --key-bindings --completion --no-update-rc --no-bash --no-fish
+  [ -f "$HOME"/.tmux.conf ] || ln -s "$MYZSH_INSTALLED_DIR"/tmux.conf "$HOME"/.tmux.conf
+  cp "$MYZSH_INSTALLED_DIR"/gitconfig-template "$HOME"/.gitconfig
   myzsh_activate_cli_tools
 }
 
 myzsh_uninstall_cli_tools() {
-  brew_uninstall gdu git wget fd ripgrep fzf htop jq gnupg tree tmux lazygit shellcheck shfmt bottom
-  brew_uninstall openssl readline sqlite3 xz zlib tcl-tk
-  brew unlink gdu
-  [ -f $HOME/.fzf.zsh ] && rm $HOME/.fzf.zsh || true
+  myzsh_brew_uninstall git wget fd ripgrep fzf htop jq tree tmux lazygit shellcheck shfmt bottom
+  myzsh_brew_uninstall openssl readline sqlite3 xz zlib tcl-tk
+  [ -f "$HOME"/.fzf.zsh ] && rm "$HOME"/.fzf.zsh
+  true
 }
 
-myzsh_activate_cli_tools () {
-  if [ -f $HOME/.fzf.zsh ]
-  then
+myzsh_activate_cli_tools() {
+  if [ -f "$HOME"/.fzf.zsh ]; then
     export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_ALT_C_COMMAND="fd --type d --hidden --follow --exclude .git"
     bindkey "ç" fzf-cd-widget
-    source $HOME/.fzf.zsh
+    source "$HOME"/.fzf.zsh
   fi
 }
 
@@ -184,29 +167,26 @@ myzsh_activate_cli_tools () {
 ################################################################################
 
 myzsh_install_antigen() {
-  brew_install_or_upgrade antigen
+  myzsh_brew_install_or_upgrade antigen
   myzsh_activate_antigen
 }
 
 myzsh_uninstall_antigen() {
-  brew_uninstall antigen
-  rm -rf $ANTIGEN_DIRECTORY &>/dev/null
+  myzsh_brew_uninstall antigen
+  rm -rf "$ANTIGEN_DIRECTORY" &>/dev/null
 }
 
 myzsh_activate_antigen() {
   type antigen &>/dev/null && return
 
-  if is_homebrew_installed
-  then
+  if myzsh_is_homebrew_installed; then
     ANTIGEN_EXEC_DIRECTORY=$(brew --prefix antigen)/share/antigen
-    if [ -f $ANTIGEN_EXEC_DIRECTORY/antigen.zsh ]
-    then
-      source $ANTIGEN_EXEC_DIRECTORY/antigen.zsh
+    if [ -f "$ANTIGEN_EXEC_DIRECTORY"/antigen.zsh ]; then
+      source "$ANTIGEN_EXEC_DIRECTORY"/antigen.zsh
       antigen use oh-my-zsh
       antigen bundle vi-mode
       antigen bundle key-bindings
-      if [ $MACHINE_TYPE = "Mac" ]
-      then
+      if [ $MACHINE_TYPE = "Mac" ]; then
         antigen bundle macos
       fi
       antigen bundle git
@@ -223,20 +203,19 @@ myzsh_activate_antigen() {
 ################################################################################
 
 myzsh_install_rtx() {
-  brew_install_or_upgrade rtx
+  myzsh_brew_install_or_upgrade rtx
   rtx_activation_file="$(brew --prefix rtx)/bin/rtx"
   eval "$($rtx_activation_file install)"
   myzsh_activate_rtx
 }
 
 myzsh_uninstall_rtx() {
-  brew_uninstall rtx
+  myzsh_brew_uninstall rtx
   rm -rf "$RTX_PREFIX"
 }
 
 myzsh_activate_rtx() {
-  if is_homebrew_installed
-  then
+  if myzsh_is_homebrew_installed; then
     rtx_activation_file="$(brew --prefix rtx)/bin/rtx"
     [ -f "$rtx_activation_file" ] && eval "$($rtx_activation_file activate zsh)"
   fi
@@ -248,7 +227,7 @@ myzsh_activate_rtx() {
 
 myzsh_install_python() {
   # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-  brew_install_or_upgrade openssl readline sqlite3 xz zlib tcl-tk pyenv
+  myzsh_brew_install_or_upgrade openssl readline sqlite3 xz zlib tcl-tk pyenv
   print_line "Installing python $1"
   pyenv install $1
   pyenv global $1
@@ -267,7 +246,7 @@ myzsh_activate_python() {
 ################################################################################
 
 myzsh_install_node() {
-  brew_install_or_upgrade n
+  myzsh_brew_install_or_upgrade n
   print_line "Installing NodeJS $1"
   n $1
   myzsh_activate_node
@@ -292,9 +271,9 @@ myzsh_activate_rust() {
 ################################################################################
 
 myzsh_install_java() {
-  install_homebrew
+  myzsh_install_homebrew
   brew tap homebrew/cask-versions
-  brew_install_or_upgrade --cask temurin17
+  myzsh_brew_install_or_upgrade --cask temurin17
   echo "Java version:"
   java -version
 }
@@ -313,137 +292,54 @@ myzsh_activate_java() {
 # Docker
 ################################################################################
 
-activate_docker() {
+myzsh_activate_docker() {
   [ -f "$DOCKER_DIRECTORY/init-zsh.sh" ] && (source "$DOCKER_DIRECTORY/init-zsh.sh" || true)
 }
 
 # Neovim
 ################################################################################
 myzsh_install_neovim() {
-  brew_install_or_upgrade homebrew/cask-fonts/font-meslo-lg-nerd-font
-  brew_install_or_upgrade neovim stylua
+  myzsh_brew_install_or_upgrade homebrew/cask-fonts/font-meslo-lg-nerd-font
+  myzsh_brew_install_or_upgrade neovim stylua
   npm install -g neovim tree-sitter-cli
   python3 -m pip install pynvim
 }
 
 myzsh_uninstall_neovim() {
-  brew_uninstall neovim stylua
+  myzsh_brew_uninstall neovim stylua
   npm uninstall -g neovim tree-sitter-cli
   python3 -m pip uninstall -y pynvim
-  rm -rf $MYZSH_INSTALLED_DIR/nvim/plugin/packer_compiled.lua
-  rm -rf $HOME/.config/nvim
-  rm -rf $HOME/.local/share/nvim
-  rm -rf $HOME/.cache/nvim
+  rm -rf "$MYZSH_INSTALLED_DIR"/nvim/plugin/packer_compiled.lua
+  rm -rf "$HOME"/.config/nvim
+  rm -rf "$HOME"/.local/share/nvim
+  rm -rf "$HOME"/.cache/nvim
 }
 
 # Mac key repeat for vscode
 ################################################################################
 
 fix_key_repeat_for_vscode() {
-  if [ $MACHINE_TYPE = Linux ]
-  then
+  if [ ! $MACHINE_TYPE = Linux ]; then
     defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-  fi
-}
-
-# KEY BINDINGS
-################################################################################
-
-myzsh_keybindings() {
-  zmodload zsh/terminfo
-
-  # From https://github.com/robbyrussell/oh-my-zsh/issues/7330
-
-  # create a zkbd compatible hash;
-  # to add other keys to this hash, see: man 5 terminfo
-  typeset -A key
-
-  key=(
-    BackSpace  "${terminfo[kbs]}"
-    Home       "${terminfo[khome]}"
-    End        "${terminfo[kend]}"
-    Insert     "${terminfo[kich1]}"
-    Delete     "${terminfo[kdch1]}"
-    Up         "${terminfo[kcuu1]}"
-    Down       "${terminfo[kcud1]}"
-    Left       "${terminfo[kcub1]}"
-    Right      "${terminfo[kcuf1]}"
-    PageUp     "${terminfo[kpp]}"
-    PageDown   "${terminfo[knp]}"
-  )
-
-  # setup key accordingly
-  [[ -n "${key[Home]}"     ]]  && bindkey  "${key[Home]}"      beginning-of-line
-  [[ -n "${key[End]}"      ]]  && bindkey  "${key[End]}"       end-of-line
-  [[ -n "${key[Insert]}"   ]]  && bindkey  "${key[Insert]}"    overwrite-mode
-  [[ -n "${key[Delete]}"   ]]  && bindkey  "${key[Delete]}"    delete-char
-  [[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"        up-line-or-history
-  [[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"      down-line-or-history
-  [[ -n "${key[Left]}"     ]]  && bindkey  "${key[Left]}"      backward-char
-  [[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"     forward-char
-  [[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"    beginning-of-buffer-or-history
-  [[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}"  end-of-buffer-or-history
-  [[ -n "${key[BackSpace]}" ]] && bindkey  "${key[BackSpace]}" backward-delete-char
-  [[ -n "${key[Home]}"      ]] && bindkey -M vicmd "${key[Home]}" beginning-of-line
-  [[ -n "${key[End]}"       ]] && bindkey -M vicmd "${key[End]}" end-of-line
-
-  # Finally, make sure the terminal is in application mode, when zle is
-  # active. Only then are the values from $terminfo valid.
-  if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-      function zle-line-init () {
-          printf '%s' "${terminfo[smkx]}"
-      }
-      function zle-line-finish () {
-          printf '%s' "${terminfo[rmkx]}"
-      }
-      zle -N zle-line-init
-      zle -N zle-line-finish
-  fi
-
-  # From old bindings
-
-  # bind UP and DOWN arrow keys
-  if typeset -f history-substring-search-up > /dev/null
-  then
-      bindkey "${key[Up]}" history-substring-search-up
-      bindkey -M vicmd "${key[Up]}" history-substring-search-up
-      bindkey -M vicmd "k"       history-substring-search-up
-      bindkey "${key[Down]}" history-substring-search-down
-      bindkey -M vicmd "${key[Down]}" history-substring-search-down
-      bindkey -M vicmd "j"       history-substring-search-down
-  fi
-}
-
-# Auto completion
-################################################################################
-
-myzsh_update_auto_completions() {
-  # We do not need compinit as oh-my-zsh will run compinit
-  if is_homebrew_installed
-  then
-    if [ $MACHINE_TYPE = Linux ]
-    then
-      FPATH="$LINUXBREW_DIRECTORY/share/zsh/site-functions:${FPATH}"
-    fi
   fi
 }
 
 # Turn off beep
 setopt nobeep
 
-myzsh_update_auto_completions # should be before antigen
+myzsh_activate_homebrew
 myzsh_activate_cli_tools
 myzsh_activate_antigen
 myzsh_activate_rtx
-# myzsh_activate_python
-# myzsh_activate_node
+myzsh_activate_python
+myzsh_activate_node
 myzsh_activate_rust
-# activate_java
-activate_docker
+myzsh_activate_java
+myzsh_activate_docker
 
-myzsh_keybindings
+# myzsh_keybindings
 
-[ -x "$HOME/.vocab" ] && $HOME/.vocab
+[ -x "$HOME/.vocab" ] && "$HOME"/.vocab
 
 [ -f "$HOME/.zshrc.local" ] && source "$HOME/.zshrc.local"
 
