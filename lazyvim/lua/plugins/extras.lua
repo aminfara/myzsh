4bf3177c95359664350f2150f67c91a2444c729f
Ali 🚶 <ali.aminfar@gmail.com>
2024-06-20 22:31:21 +1000

    lazyvim add golang support

--- lazyvim/lua/plugins/extras.lua
+++ lazyvim/lua/plugins/extras.lua
@@ -1,3 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.test.core" },
+  { import = "lazyvim.plugins.extras.lang.go" },
 }
