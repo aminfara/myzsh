a922620610ef8e0e4e003348bdb9d1e939550cb5
Ali 🚶 <ali.aminfar@gmail.com>
2024-06-23 18:30:40 +1000

    Lazyvim fix golangci-lint lazyload

--- lazyvim/lua/plugins/go.lua
+++ lazyvim/lua/plugins/go.lua
@@ -5,32 +5,12 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local lspconfig = require("lspconfig")
-      local configs = require("lspconfig.configs")
-      if not configs.golangcilsp then
-        configs.golangcilsp = {
-          default_config = {
-            cmd = { "golangci-lint-langserver" },
-            root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
-            init_options = {
-              command = {
-                "golangci-lint",
-                "run",
-                "--enable-all",
-                "--disable",
-                "lll",
-                "--out-format",
-                "json",
-                "--issues-exit-code=1",
-              },
-            },
-          },
-        }
-      end
-      lspconfig.golangci_lint_ls.setup({
-        filetypes = { "go", "gomod" },
-      })
-    end,
+    opts = {
+      servers = {
+        golangci_lint_ls = {
+          filetypes = { "go", "gomod" },
+        },
+      },
+    },
   },
 }
