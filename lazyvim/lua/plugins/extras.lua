f8e310d635f1493ff9e85cf73d10284caa3066b7
Ali 🚶 <ali.aminfar@gmail.com>
2024-09-20 23:28:32 +1000

    Vim configuraiton enable markdown

--- lazyvim/lua/plugins/extras.lua
+++ lazyvim/lua/plugins/extras.lua
@@ -8,6 +8,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
