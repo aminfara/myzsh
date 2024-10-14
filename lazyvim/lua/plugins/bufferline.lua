8d968c523769bb84e6228e47bd894c9974139a6b
Ali 🚶 <ali.aminfar@gmail.com>
2024-10-14 19:51:16 +1100

    lazyvim disable bufferline italic

--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -1,3 +1,30 @@
 return {
-  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        show_buffer_close_icons = false,
+      },
+      highlights = {
+        buffer_selected = { italic = false },
+        numbers_selected = { italic = false },
+        diagnostic_selected = { italic = false },
+        hint_selected = { italic = false },
+        hint_diagnostic_selected = { italic = false },
+        info_selected = { italic = false },
+        info_diagnostic_selected = { italic = false },
+        warning_selected = { italic = false },
+        warning_diagnostic_selected = { italic = false },
+        error_selected = { italic = false },
+        error_diagnostic_selected = { italic = false },
+        duplicate_selected = { italic = false },
+        duplicate_visible = { italic = false },
+        duplicate = { italic = false },
+        pick_selected = { italic = false },
+        pick_visible = { italic = false },
+        pick = { italic = false },
+      },
+    },
+  },
 }
