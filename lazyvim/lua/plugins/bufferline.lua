1f2e10210c6d84983b7cda86cb64c1ef540685a3
Ali 🚶 <ali.aminfar@gmail.com>
2024-10-14 22:04:13 +1100

    lazy better bufferline config

--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -2,29 +2,10 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    opts = {
-      options = {
-        show_buffer_close_icons = false,
-      },
-      highlights = {
-        buffer_selected = { italic = false },
-        numbers_selected = { italic = false },
-        diagnostic_selected = { italic = false },
-        hint_selected = { italic = false },
-        hint_diagnostic_selected = { italic = false },
-        info_selected = { italic = false },
-        info_diagnostic_selected = { italic = false },
-        warning_selected = { italic = false },
-        warning_diagnostic_selected = { italic = false },
-        error_selected = { italic = false },
-        error_diagnostic_selected = { italic = false },
-        duplicate_selected = { italic = false },
-        duplicate_visible = { italic = false },
-        duplicate = { italic = false },
-        pick_selected = { italic = false },
-        pick_visible = { italic = false },
-        pick = { italic = false },
-      },
-    },
+    opts = function(_, opts)
+      local bufferline = require("bufferline")
+      opts.options.style_preset = bufferline.style_preset.no_italic
+      return opts
+    end,
   },
 }
